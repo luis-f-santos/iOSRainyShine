3e5c12baaaa772ffaa2ffab4c8fc01fb6dd6d030
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2018-09-24 21:29:35 -0500

    Initial Commit

---
+++ RainyShine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainyShine
+//
+//  Created by Luis Santos on 9/24/18.
+//  Copyright © 2018 Luis Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
