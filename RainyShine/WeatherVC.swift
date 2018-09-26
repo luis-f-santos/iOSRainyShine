48586e993ef5d0944c85c18f1a3e4f8dc310c769
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2018-09-26 01:54:38 -0500

    Adding IBOutlets for labels and images plus UITable

--- RainyShine/WeatherVC.swift
+++ RainyShine/WeatherVC.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class WeatherVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
