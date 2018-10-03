f58a4e359c961db807330b61890581897dfaeee2
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2018-10-03 17:15:33 -0500

    adding pics and parsing JSON for top of UI

--- RainyShine/WeatherVC.swift
+++ RainyShine/WeatherVC.swift
@@ -18,16 +18,16 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var currentWeatherTypeLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
-    var currentWeather = CurrentWeather()
+    var currentWeather:CurrentWeather!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-        
+        currentWeather = CurrentWeather()
         currentWeather.downloadWeatherDetails {
-            //Setup UI to load download
+            self.updateMainUI()
         }
     }
 
@@ -43,6 +43,16 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
         
         return cell
+    }
+    
+    func updateMainUI() {
+        
+        dateLabel.text = currentWeather.date
+        currentTempLabel.text = "\(currentWeather.currentTemp)°"
+        currentWeatherTypeLabel.text = currentWeather.weatherType
+        cityLabel.text = currentWeather.cityName
+        currentWeatherImage.image = UIImage(named: currentWeather.weatherType)
+        
     }
 
 }
